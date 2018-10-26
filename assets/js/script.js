03e9c075807ac96aea040428bbc43df93137ee6f
Rasalful <willygaudin@gmail.com>
2018-10-26 14:19:22 +0200

    modif footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
 var acc = document.getElementsByClassName("accordion");
 for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
+    acc[i].addEventListener("click", function () {
         this.classList.toggle("active");
         var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
+        if (panel.style.height === "auto") {
+            panel.style.height = "0";
+            var accordeon = new Audio();
+            accordeon.src = "assets/sound/accordeon.mp3";
+            accordeon.play();
         } else {
-            panel.style.display = "block";
+            panel.style.height = "auto";
+            var accordeon3 = new Audio();
+            accordeon3.src = "assets/sound/accordeon2.wav";
+            accordeon3.play();
         }
     });
 }
