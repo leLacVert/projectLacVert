0cb4e32a87a0717f4ca37cec80395d1180850b9f
Olivier Sauvage <oliver0062@yahoo.fr>
2018-10-29 01:08:52 +0100

    adjust nav

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,40 +1,19 @@
-/*
-var unroll=document.getElementsByClassName('unroll');
+// document.getElementById('menu-toggle').addEventListener('click', function () {
+//   var burger = document.getElementsByTagName('nav');
 
-for(var i=0;i<unroll.length;i++){
-	
-	unroll[i].addEventListener('click',function(){
-		
-		var liHeight = this.offsetHeight;
-		
-		for(var i=0;i<unroll.length;i++){
-			unroll[i].style.height='50px';
-		}
-		
-		if(liHeight==50){
-			this.style.height='300px';
-		}
-	},false); 
-};
------------------------------------------*/
-function toggleClass() {
-  var drop = document.getElementsByClassName('nav_up');
-  for (var i = 0; i < drop.length; i++) {
-    var drops = drop[i];
-  }
-  if (drops.classList.contains('nav_down')) {
-    drops.classList.remove('nav_down');
-  } else {
-    drops.classList.add('nav_down');
-  }
-}
+//   if (burger.classList.contains('active')) {
+//     burger.classList.remove('active');
+//   } else {
+//     burger.classList.add('active');
+//   }
+// }, false);
 
-var link = document.getElementsByClassName('link_click');
-for (var i = 0; i < link.length; i++) {
-  var links = link[i];
-  links.addEventListener('click', function () {
-    toggleClass();
-  },
-  false
-  );
-}
+$(document).ready(function () {
+  $('#menu-toggle').click(function () {
+    $('nav').toggleClass('active')
+  })
+  $('ul li').click(function () {
+    $(this).siblings().removeClass('active');
+    $(this).toggleClass('active');
+  })
+})
